64759c92670f87874d5a4dc6ccdbf1f1dcdb71f0
thanaa1812 <94175461+thanaa1812@users.noreply.github.com>
2024-03-23 17:51:48 +0300

    Update streamlit_app_v2.py

--- streamlit_app_v2.py
+++ streamlit_app_v2.py
@@ -41,7 +41,7 @@ os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?",  icon='⚠️'}]
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
 # Display or clear chat messages
 for message in st.session_state.messages:
